30664a5c40796030d6192b8932655df4e3b89fc0
Angel <Angel.Dong@colorado.edu>
2024-06-12 21:43:49 -0700

    add button is changed to input boxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import ExpenseCategory from './components/ExpenseCategory';
 import Button from "./components/Button";
+import { useState } from 'react';
 
 const categories = ["misc", "fun"]
 
@@ -26,21 +27,50 @@ const dummyItems = [
 ]
 
 function App() {
+
+  const [isOpenInput, setOpenInput] = useState(false);
+  const [newItem, setNewItem] = useState({
+    "purchaseItem": "",
+    "purhcaseAmount": "",
+    "purhcaseDate": "",
+    "budgetCategory": ""
+  })
+
   const addOnclick = () => {
     console.log("Add is clicked!")
+    setOpenInput(true);
+
+  }
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    console.log("Onsubmit clicked!")
+    setOpenInput(false)
+    setNewItem(...dummyItems, newItem)
 
   }
   return (
 
     <div className="App">
       <h1>Expense Tracker</h1>
+      {!isOpenInput && <Button name="Add Item" colorClass={"btn-add"} onClick={addOnclick} />}
+
+      {isOpenInput &&
+      <form onSubmit={handleOnSubmit}>
+        <label>Purchased Item:  <input name="item" value={newItem.purchaseItem} /></label>
+        <label>Purchased Amount:  <input name="amount" value={newItem.purhcaseAmount} /></label>
+        <label>Purchased Date:  <input name="date" value={newItem.purhcaseDate} /></label>
+        <label>Purchased Category:  <input name="category" value={newItem.budgetCategory} /></label>
+        <button type='submit' value='Submit'>Submit</button> 
+      </form>   
+      }
       {categories.map((category, index) =>
         <ExpenseCategory
           items={dummyItems}
           category={category}
         />)}
       <div className="additm-container">
-        <Button name="Add Item" colorClass={"btn-add"} onClick={addOnclick} />
+
       </div>
     </div>
   );
